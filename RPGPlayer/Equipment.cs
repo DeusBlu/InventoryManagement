eb1e19dfc996e18c1945bdddf761295dc28faa7b
Desmond Jenkins <djenkins@bitstew.com>
2016-04-14 11:33:04 -0700

    fixed logic errors in the equip method

--- RPGPlayer/Equipment.cs
+++ RPGPlayer/Equipment.cs
@@ -25,21 +25,24 @@ namespace RPGPlayer
             // will be 0 if the equipment cannot go in that slot
             if((equip.EquipSlot & slot) != 0)
             {
-                // will be 0 if there is nothing equipped to that slot
-                if((equip.EquipSlot & equipState) != 0)
+                // will be false if there is nothing equipped to that slot
+                if(IsEquipped(slot))
                 {
                     // is the item to equip two handed
-                    if(equip.EquipSlot == EquipSlots.TWO_HANDED)
+                    if((equip.EquipSlot & EquipSlots.TWO_HANDED) != 0)
                     {
-                        // will be 0 if a two handed sword is not equipped
-                        if ((equipState & EquipSlots.TWO_HANDED) == 0)
+                        // will be false if a two handed weapon is not equipped
+                        if (!IsEquipped(EquipSlots.TWO_HANDED))
                         {
-                            // will be 0 if neither hand is equipped
-                            if ((equipState & EquipSlots.BOTH_HANDS) == 0)
+                            // Here we need to know if both hands are equipped or only one
+                            // returns true is both hands equipped with item
+                            if (IsEquipped(EquipSlots.BOTH_HANDS))
                             {
                                 // evaluates to true if the inventory has enough space to return the item
                                 if (!playerInventory.IsFull())
                                 {
+                                    // both hands have to have something in them and cannot be a 2H item by here
+                                    // so we add offhand to the inventory and place main hand on the cursor by returning it
                                     playerInventory.AddItemToLocation(playerInventory.GetFirstEmptyIndex(), (Item)equipment[EquipSlots.OFF_HAND]);
                                     equipped = Unequip(EquipSlots.MAIN_HAND);
                                     equipGear(equip, slot);
@@ -47,14 +50,16 @@ namespace RPGPlayer
                             }
                             else
                             {
-                                if (slot == EquipSlots.MAIN_HAND)
+                                // by this point we can assume that the item being
+                                // equipped is a 2H weapon and only one hand is used
+                                if (IsEquipped(EquipSlots.MAIN_HAND))
                                 {
-                                    equipped = Unequip(EquipSlots.OFF_HAND);
+                                    equipped = Unequip(EquipSlots.MAIN_HAND);
                                     equipGear(equip, slot);
                                 }
                                 else
                                 {
-                                    equipped = Unequip(EquipSlots.MAIN_HAND);
+                                    equipped = Unequip(EquipSlots.OFF_HAND);
                                     equipGear(equip, slot);
                                 }
                             }
@@ -79,6 +84,7 @@ namespace RPGPlayer
             return equipped;
         }
 
+        /* unequips items updates the equip state and returns the unequipped item*/
         public Gear Unequip(EquipSlots slot)
         {
             Gear equipped = (Gear)equipment[slot];
@@ -90,27 +96,33 @@ namespace RPGPlayer
             return equipped;
         }
 
+        /* equips gear and updates the equipState at the same time to
+        keep these two items in sync */
         private void equipGear(Gear equip, EquipSlots slot)
         {
             equipment.Add(slot, equip);
             equipState = equipState | equip.EquipSlot;
         }
 
+        /* returns an enumerator for working with the gear set in game */
         public IDictionaryEnumerator GetIterator()
         {
             return equipment.GetEnumerator();
         }
 
+        /* returns the gear in the specified equip slot */
         public Gear GetGearBySlot(EquipSlots slot)
         {
             return (Gear)equipment[slot];
         }
 
+        /* returns the current equip state to the user for testing in game logic */
         public EquipSlots GetCurrentEquipState()
         {
             return equipState;
         }
 
+        /* generates new enum labels so you can create your own equipment location lists */
         public EquipSlots GenerateEquipSlot()
         {
             EquipSlots slot = (EquipSlots)(1 << nextSlot);
@@ -118,6 +130,7 @@ namespace RPGPlayer
             return slot;
         }
 
+        /* returns true if the spot being equipped to has equipment in it */
         public bool IsEquipped(EquipSlots slot)
         {
             return equipState.HasFlag(slot);
